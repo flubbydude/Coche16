00ed2e6775c4041f150fa1adb08a78ce42f00b09
Ben Short <ben.is.not.short@gmail.com>
2016-02-17 10:30:43 -0500

    Cartesian Vector

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,21 +1,14 @@
-/*
- * Vector.cpp
- *
- *  Created on: Feb 17, 2016
- *      Author: Benjamin
- */
-
 #include "Vector.h"
 #include <Math.h>
 
-Vector* Vector::operator+(Vector* v){
-	float x1 = this->magnitude*cos(this->angle);
-	float y1 = this->magnitude*sin(this->angle);
-	float x2 = v->magnitude*cos(v->angle);
-	float y2 = v->magnitude*sin(v->angle);
+Vector Vector::operator+(Vector& v){
+	float x1 = magnitude*cos(angle);
+	float y1 = magnitude*sin(angle);
+	float x2 = v.magnitude*cos(v.angle);
+	float y2 = v.magnitude*sin(v.angle);
 	float xTotal = x1+x2;
 	float yTotal = y1+y2;
 	float degrees = atan2(yTotal,xTotal);
 	float magnitude = sqrt(xTotal*xTotal+yTotal*yTotal);
-	return new Vector(magnitude, degrees);
+	return Vector(magnitude, degrees);
 }
